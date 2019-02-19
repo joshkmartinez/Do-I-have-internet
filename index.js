1e33319f6035024cc99426fafdd9b28d98853ee1
Joshua Martinez <joshkmartinez@gmail.com>
2019-02-18 16:33:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var app = express()
 app.get('/ping', (req, res, next) => {
   res.send('pong')
 })
+
+app.get('/help', (req, res, next) => {
+  res.send('This tells you if you have internet or not.')
+})
+
 app.get('/', function(req, res) {
   res.send('no')
 })
