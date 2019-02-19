6e0026e8d8e347098d9d25730946364190b33930
Joshua Martinez <joshkmartinez@gmail.com>
2019-02-18 18:30:02 -0800

    general updates

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 var express = require('express')
 var app = express()
-const responses = ['No', 'Not really', 'Do you?', 'I hope so.',"No. You do not have internet","Error. Please connect to the internet and try again.","Error reaching internet. Try Again.","Are you on the moon?", "No, why?"]
+const responses = [
+  'No',
+  'Not really',
+  'Do you?',
+  'I hope so.',
+  'No. You do not have internet',
+  'Connect to the internet and try again.',
+  'Error reaching internet. Try Again.',
+  'Are you on the moon?',
+  'No, why?',
+  "Of course not."
+]
 app.get('/ping', (req, res, next) => {
   res.send('pong')
 })
 
 app.get('/help', (req, res, next) => {
-  res.send('This very useful service tells you wether you have internet, or not.')
+  res.send(
+    'This very useful service tells you wether you have internet, or not.'
+  )
 })
 
 app.get('/', function(req, res) {
-  res.send(responses[Math.floor(Math.random() * responses.length)])
+  if (9 === Math.floor(Math.random() * 9) + 1) {
+    res.redirect('https://rickroll.now.sh')
+  } else {
+    res.send(responses[Math.floor(Math.random() * responses.length)])
+  }
 })
 app.get('/responses', function(req, res) {
   res.send(responses)
